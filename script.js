0c4a158a9969f1224afe88f2cb6a3ba4b244577b
Remi <kilona.soleis@gmail.com>
2025-05-15 09:11:19 +0200

    JS update

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ button.addEventListener('click', checkForPalindrome);
 
 function checkForPalindrome() {
     const text = input.value;
-    const cleanedText = text.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const cleanedText = text.replace(/[^a-zA-Z0-9çÇ]/g, '').toLowerCase();
     const reversedText = cleanedText.split('').reverse().join('');
 
-    if (cleanedText === reversedText) {
+    if (cleanedText.length <= 1) {
+        result.textContent = `"${text}" n'est pas un palindrome (texte trop court).`;
+    } else if (cleanedText === reversedText) {
         result.textContent = `"${text}" est un palindrome !`;
     } else {
         result.textContent = `"${text}" n'est pas un palindrome.`;
